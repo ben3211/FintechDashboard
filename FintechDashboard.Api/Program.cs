44330d1363d81cda4c0972ca33297f6e7c40da64
benoit saby <benoit.saby@hotmail.fr>
2025-06-23 23:18:56 +0200

    NavMenu / Create StockViewer page / Clear struture

--- FintechDashboard.Api/Program.cs
+++ FintechDashboard.Api/Program.cs
@@ -7,19 +7,8 @@ builder.Services.AddHttpClient<StockService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
 app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ FintechDashboard.Web/Pages/StockViewer.razor.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Json;
+
+namespace FintechDashboard.Web.Pages
+{
+    public partial class StockViewer
+    {
+    private string Symbol = "AAPL";
+        private decimal? Price = null;
+        private bool Loading = false;
+        private bool HasError = false;
+
+        private async Task FetchPrice()
+        {
+            Loading = true;
+            HasError = false;
+            Price = null;
+
+            try
+            {
+                var result = await Http.GetFromJsonAsync<StockResult>($"https://localhost:7283/api/stock/{Symbol}");
+                Price = result?.Price;
+            }
+            catch
+            {
+                HasError = true;
+            }
+            finally
+            {
+                Loading = false;
+            }
+        }
+
+        public class StockResult
+        {
+            public string? Symbol { get; set; }
+            public decimal Price { get; set; }
+        }
+    }
+}
